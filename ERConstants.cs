042dd6a95338db4fbd8f3b33b0634f1e54352745
Роман Эйрих <r.eirih@ya.ru>
2024-09-06 13:58:58 +0700

    Add new methods and couple of constants

--- ERConstants.cs
+++ ERConstants.cs
@@ -35,7 +35,7 @@ namespace Elden_Ring_Auto_Bingo
             long addr = er.FindPatternInModule(er.ERProcess.MainModule, pattern);
             return addr + er.ReadInt(addr + 3) + 7;
         }
-        public int GetChrcount()
+        public int GetChrCount()
         {
             long worldchrman = er.ReadLong(GetWorldchrman());
             long begin = er.ReadLong(worldchrman + 0x1F1B8);
@@ -43,28 +43,76 @@ namespace Elden_Ring_Auto_Bingo
             int chr_count = (int)(end - begin) / 8;
             return chr_count;
         }
-        public long GetChrset()
+        public long GetChrSet()
         {
             long worldchrman = er.ReadLong(GetWorldchrman());
             long chrset = er.ReadLong(worldchrman + 0x1F1B8);
             return chrset;
         }
+        public long GetPlayerAnim()
+        {
+            long worldchrman = er.ReadLong(GetWorldchrman());
+            long addr = er.GetOffsets(worldchrman, [0x10EF8, 0, 0x58, 0x10, 0x190, 0x18, 0x40]);
+            return addr;
+        }
+        public long GetCurrentBoss()
+        {
+            return er.GetOffsets(er.ERProcess.MainModule.BaseAddress, [0x03D5DF58, 0xCC]);
+        }
+        public long GetEnemyResistances(long enemyaddr)
+        {
+            return er.GetOffsets(enemyaddr, [0x190, 0x20, 0x10]);
+        }
+        public long GetEnemyHP(long enemyaddr)
+        {
+            return er.ReadInt(er.GetOffsets(enemyaddr, [0x190, 0, 0x138]));
+        }
+        public int GetEnemyCurrentAnim(long enemyaddr)
+        {
+            return er.ReadInt(er.GetOffsets(enemyaddr, [0x190, 0x18, 0x40]));
+        }
+        public bool IsBossDeadAF(long bossaddr)
+        {
+            return er.ReadByte(er.GetOffsets(bossaddr, [0x58, 0xC8, 0x24])) == 1;
+
+        }
+        public bool IsStatusEffectUsed(long boss_id)
+        {
+            long resistance = GetEnemyResistances(boss_id);
+            for (long addr = resistance; addr < resistance + 7 * 4; addr += 4)
+            {
+                long currentResistance = er.ReadInt(addr);
+                long maxResistance = er.ReadInt(addr + 0x1C);
+                if (currentResistance != maxResistance)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
         public long FindAddrById(long enemyid)
         {
-            int chr_count = GetChrcount();
+            int chr_count = GetChrCount();
             long enemyaddr = 0;
-            long chrset = GetChrset();
+            long chrset = GetChrSet();
             for (int i = 0; i < chr_count; i++)
             {
                 enemyaddr = er.ReadLong(chrset + i * 0x10);
                 int paramid = er.ReadInt(enemyaddr + 0x60);
                 if (paramid == enemyid)
                 {
-                    Console.WriteLine(enemyaddr.ToString("x8"));
                     return enemyaddr;
                 }
             }
             return 0;
         }
+        public const int MARGIT_PARAM_ID = 21300014;
+        public const int GODSKIN_NOBLE_VOLCANO_PARAM_ID = 35700038;
+
+        public const int MARGIT_PARRY_ANIM1 = 2008500;
+        public const int MARGIT_PARRY_ANIM2 = 2020015;
+
+
+
     }
 }
