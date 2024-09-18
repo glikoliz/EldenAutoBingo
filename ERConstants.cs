bddc3149071ff1b077dee816da886fc389727b7d
Роман Эйрих <r.eirih@ya.ru>
2024-09-18 19:37:21 +0700

    Add new constant and fix some offsets

--- ERConstants.cs
+++ ERConstants.cs
@@ -51,6 +51,12 @@
             long addr = er.GetOffsets(worldchrman, [0x10EF8, 0, 0x58, 0x10, 0x190, 0x18, 0x40]);
             return addr;
         }
+        public long GetPlayerDamageRate()
+        {
+            long worldchrman = er.ReadLong(GetWorldchrman());
+            long addr = er.GetOffsets(worldchrman, [0x10EF8, 0, 0x190, 0x98, 0x44]);
+            return addr;
+        }
         public long GetCurrentBoss()
         {
             return er.GetOffsets(er.ERProcess.MainModule.BaseAddress, [0x03D5DF58, 0xCC]);
@@ -73,7 +79,7 @@
         }
         public int GetCurrentWeaponId()
         {
-            long addr = er.GetOffsets(er.ReadLong(er.ERProcess.MainModule.BaseAddress + 0x03D63418), [0x40, 0x10, 0, 0x10, 0x1F0]);
+            long addr = er.GetOffsets(er.ReadLong(er.ERProcess.MainModule.BaseAddress + 0x03B12E30), [0x0, 0x58, 0x18, 0x108, 0x0, 0xA8, 0x1F0]);
             return er.ReadInt(addr);
         }
         public bool FindExplosiveFlaskEffect()
@@ -132,15 +138,33 @@
         }
 
         public const int MARGIT_PARAM_ID = 21300014;
+        public const int MORGOTT_PARAM_ID = 21300534;
+        public const int RYKARD_PARAM_ID1 = 47100038;
+        public const int RYKARD_PARAM_ID2 = 47101038;
         public const int GODRICK_PARAM_ID = 47500014;
-        public const int GODSKIN_NOBLE_VOLCANO_PARAM_ID = 35700038;
-        public const int RENALLA_PARAM_ID = 20310024;
         public const int RADAHN_PARAM_ID = 47300040;
+        public const int RENALLA_PARAM_ID = 20310024;
+        public const int REAL_MOHG_PARAM_ID = 48000068;
+        public const int MELANIA_PARAM_ID = 21200056;
+        public const int REGAL_SPIRIT_PARAM_ID = 46700065;
+        public const int ASTEL_PARAM_ID = 46200062;
+        public const int FORTISSAX_PARAM_ID = 45110066;
+        public const int PLACIDUSAX_PARAM_ID = 45200072;
+        public const int FIRE_GIANT_PARAM_ID1 = 47600050;
+        public const int FIRE_GIANT_PARAM_ID2 = 47601050;
+        public const int MALIKETH_PARAM_ID1 = 21100072;
+        public const int MALIKETH_PARAM_ID2 = 21101072;
+        public const int GODFREY_PARAM_ID = 47200134;
+        public const int ELDEN_BEAST_PARAM_ID = 22000078;
+
+        public const int GODSKIN_NOBLE_VOLCANO_PARAM_ID = 35700038;
         public const int MIMIC_PARAM_ID = 526100965;
         public const int SOLDIER_OF_GOD_PARAM_ID = 43113906;
         public const int LIMGRAVE_TREE_SENTINTEL_PARAM_ID = 32510010;
         public const int AGHEEL_PARAM_ID = 45000010;
-
+        public static int[] REMEMBRANCE_BOSSES_PARAM_ID = { GODRICK_PARAM_ID, RENALLA_PARAM_ID, RADAHN_PARAM_ID, MORGOTT_PARAM_ID, 
+            RYKARD_PARAM_ID1, RYKARD_PARAM_ID2, REAL_MOHG_PARAM_ID, MELANIA_PARAM_ID, REGAL_SPIRIT_PARAM_ID, ASTEL_PARAM_ID, FORTISSAX_PARAM_ID, 
+            PLACIDUSAX_PARAM_ID, FIRE_GIANT_PARAM_ID1, FIRE_GIANT_PARAM_ID2, MALIKETH_PARAM_ID1,MALIKETH_PARAM_ID2, GODFREY_PARAM_ID, ELDEN_BEAST_PARAM_ID};
 
         public const int NEPHELI_PARAM_ID = 533340014;
 
