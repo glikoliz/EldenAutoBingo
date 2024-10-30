b4f4e0a353b6db655f341e238aa7ad722696aeb9
Роман Эйрих <r.eirih@ya.ru>
2024-10-30 07:07:41 +0700

    4 new effects

--- BingoSquares.cs
+++ BingoSquares.cs
@@ -6,12 +6,10 @@
         private static ERConstants constants = new ERConstants(er);
         private static long eventFlag = er.ReadLong(constants.GetEventflagman());
 
-        private static bool IsBossDead(int offset, int bitPosition)
+        private static bool CheckFlag(int offset, int bitPosition)
         {
             long boss = er.GetOffsets(eventFlag, [0x28, offset]);
-            if ((er.ReadByte(boss) & (1 << bitPosition)) != 0)
-                return true;
-            return false;
+            return (er.ReadByte(boss) & (1 << bitPosition)) != 0;
         }
         private static int KilledBossesCount((int Boss, int Byte, int Count)[] bossesData)
         {
@@ -25,35 +23,37 @@
             }
             return count;
         }
-        public static bool LeonineDead() => IsBossDead(0xA7B7F, 7);
-        public static bool GoldenHippoDead() => IsBossDead(0x477, 7);
-        public static bool MidraDead() => IsBossDead(0x90, 3);
-        public static bool RadahnDead() => IsBossDead(0x8E, 0);
-        public static bool DancingLionDead() => IsBossDead(0x8E, 3) || IsBossDead(0x174F3, 7);
-        public static bool AncientSenessaxDead() => IsBossDead(0x2DCD, 2);
-        public static bool BlackKnightDead() => IsBossDead(0x2DE1, 2) || IsBossDead(0x2DE0, 4);
-        public static bool JoriDead() => IsBossDead(0x91, 6);
-        public static bool LamenterDead() => IsBossDead(0x9F, 2);
-        public static bool DeathKnightDead() => IsBossDead(0x9E, 1) || IsBossDead(0x9E, 0);
-        public static bool MariggaDead() => IsBossDead(0x2DD2, 2);
-        public static bool BloodfiendDead() => IsBossDead(0xA0, 7);
-        public static bool PutrescentKnightDead() => IsBossDead(0x8F, 3);
-        public static bool DryleafDaneDead() => IsBossDead(0x25689, 7); //test this
-        public static bool ScadutreeAvatarDead() => IsBossDead(0x91, 5);
-        public static bool RellanaDead() => IsBossDead(0x47C, 1);
-        public static bool MetyrDead() => IsBossDead(0x478, 4);
-        public static bool MessmerDead() => IsBossDead(0x477, 5);
-        public static bool SentinelDuoDead() => IsBossDead(0x2DDF, 1);
-        public static bool RominaDead() => IsBossDead(0x91, 7);
-        public static bool DragonManDead() => IsBossDead(0xA0, 6);
-        public static bool DeathRiteBirdDead() => IsBossDead(0x1ABA3, 7);
-        public static bool RedBearDead() => IsBossDead(0x2DD9, 3);
-        public static bool FrenzyNPCDead() => IsBossDead(0x3310A, 3);
-        public static bool LogurBeastClawDead() => IsBossDead(0x1AEE8, 4);
-        public static bool BolsDead() => IsBossDead(0x1BC7C, 1);
-        public static bool CarianKnightDead() => IsBossDead(0x1BC83, 7);
-        public static bool LedaDead() => IsBossDead(0x3B9, 6);
-        public static bool DemiSwordsmanDead() => IsBossDead(0x9F, 4); //1ADAF 4 for non-boss
+        public static bool LeonineDead() => CheckFlag(0xA7B7F, 7);
+        public static bool GoldenHippoDead() => CheckFlag(0x477, 7);
+        public static bool MidraDead() => CheckFlag(0x90, 3);
+        public static bool RadahnDead() => CheckFlag(0x8E, 0);
+        public static bool DancingLionDead() => CheckFlag(0x8E, 3) || CheckFlag(0x174F3, 7);
+        public static bool AncientSenessaxDead() => CheckFlag(0x2DCD, 2);
+        public static bool BlackKnightDead() => CheckFlag(0x2DE1, 2) || CheckFlag(0x2DE0, 4);
+        public static bool JoriDead() => CheckFlag(0x91, 6);
+        public static bool LamenterDead() => CheckFlag(0x9F, 2);
+        public static bool DeathKnightDead() => CheckFlag(0x9E, 1) || CheckFlag(0x9E, 0);
+        public static bool MariggaDead() => CheckFlag(0x2DD2, 2);
+        public static bool BloodfiendDead() => CheckFlag(0xA0, 7);
+        public static bool PutrescentKnightDead() => CheckFlag(0x8F, 3);
+        public static bool DryleafDaneDead() => CheckFlag(0x25689, 7); //test this
+        public static bool ScadutreeAvatarDead() => CheckFlag(0x91, 5);
+        public static bool RellanaDead() => CheckFlag(0x47C, 1);
+        public static bool MetyrDead() => CheckFlag(0x478, 4);
+        public static bool MessmerDead() => CheckFlag(0x477, 5);
+        public static bool SentinelDuoDead() => CheckFlag(0x2DDF, 1);
+        public static bool RominaDead() => CheckFlag(0x91, 7);
+        public static bool DragonManDead() => CheckFlag(0xA0, 6);
+        public static bool DeathRiteBirdDead() => CheckFlag(0x1ABA3, 7);
+        public static bool RedBearDead() => CheckFlag(0x2DD9, 3);
+        public static bool FrenzyNPCDead() => CheckFlag(0x3310A, 3);
+        public static bool LogurBeastClawDead() => CheckFlag(0x1AEE8, 4);
+        public static bool BolsDead() => CheckFlag(0x1BC7C, 1);
+        public static bool CarianKnightDead() => CheckFlag(0x1BC83, 7);
+        public static bool LedaDead() => CheckFlag(0x3B9, 6);
+        public static bool DemiSwordsmanDead() => CheckFlag(0x9F, 4); //1ADAF 4 for non-boss
+        public static bool YmirDead() => CheckFlag(0x2F058, 7);
+        public static bool QueelignDead() => CheckFlag(0x17AF13, 3);
         public static bool MausoleumsCompleted()        
         {
             (int Boss, int Byte, int Count)[] bossesData =
@@ -154,9 +154,9 @@
         }
         public static bool BearLionHippoDead()
         {
-            bool Bear = IsBossDead(0x2DDD, 5) || IsBossDead(0xDB24, 7);
-            bool Lion = IsBossDead(0x8E, 3) || IsBossDead(0x174F3, 7);
-            bool Hippo = IsBossDead(0x477, 7);
+            bool Bear = CheckFlag(0x2DDD, 5) || CheckFlag(0xDB24, 7);
+            bool Lion = CheckFlag(0x8E, 3) || CheckFlag(0x174F3, 7);
+            bool Hippo = CheckFlag(0x477, 7);
             return Bear && Lion && Hippo;
         }
         public static bool KnightBossesDead()
@@ -180,6 +180,56 @@
             bool isDead = er.ReadByte(handDeadAddr) == 1;
             return isDead;
         }
+        public static bool HipposDead()
+        {
+            (int Boss, int Byte, int Count)[] bossesData =
+{
+                (0x2ECAE, 3, 1),
+                (0x2ECAF, 1, 1),
+                (0x1AB64, 3, 1),
+                (0x174B3, 5, 1),
+                (0x477, 7, 1),
+            };
+            return KilledBossesCount(bossesData) >= 2;
+        }
+        public static bool BayleWithIgonDead()
+        {
+            long bossAddr = constants.GetCurrentBoss();
+            int currentBossId = er.ReadInt(bossAddr);
+            long currentBossAddr = constants.FindAddrById(currentBossId);
+            long isDeadAddr = constants.IsBossDeadAddr(currentBossAddr);
+            while (currentBossId == (int)ERConstants.BossId.Bayle)
+            {
+                currentBossId = er.ReadInt(bossAddr);
+                if (er.ReadByte(isDeadAddr) == 1)
+                    return CheckFlag(0xC04E6, 7); //check if igon was summoned 
+                Thread.Sleep(200);
+            }
+            return false;
+
+        }
+        public static bool BossKilledWithousStatus()
+        {
+            long bossAddr = constants.GetCurrentBoss();
+            long currentBoss = er.ReadInt(bossAddr);
+            long currentBossAddr = constants.FindAddrById(currentBoss);
+            bool check;
+            bool isDead;
+            while (currentBoss != 0)
+            {
+                currentBoss = er.ReadInt(bossAddr);
+                check = constants.IsStatusEffectUsed(currentBossAddr);
+                isDead = er.ReadByte(constants.IsBossDeadAddr(currentBossAddr)) == 1;
+                if (check)
+                    return false;
+                else if (isDead)
+                    return true;
+                Thread.Sleep(1000);
+            }
+            return false;
+        }
+
+
         public static bool FurnaceGolemsDead()
         {
             (int Boss, int Byte, int Count)[] bossesData =
@@ -189,5 +239,43 @@
             };//TODO
             return KilledBossesCount(bossesData) >= 5;
         }
+        public static bool RemembranceBossHitless() //TODO
+        {
+            long bossaddr = constants.GetCurrentBoss();
+            int currentbossid = er.ReadInt(bossaddr);
+            long currentbossaddr = constants.FindAddrById(currentbossid);
+            bool isdead;
+            float damageRate = er.ReadFloat(constants.GetPlayerDamageRate());
+            float prevDamageRate = damageRate;
+            while (ERConstants.REMEMBRANCE_BOSSES_PARAM_ID.Contains(currentbossid))
+            {
+                damageRate = er.ReadFloat(constants.GetPlayerDamageRate());
+                currentbossid = er.ReadInt(bossaddr);
+                Console.WriteLine(currentbossaddr);
+                isdead = er.ReadByte(constants.IsBossDeadAddr(currentbossaddr)) == 1;
+                if (damageRate != prevDamageRate && damageRate != 0)
+                {
+                    Console.WriteLine("Hit");
+                    Thread.Sleep(1000);
+                    return false;
+                }
+                if (isdead)
+                {
+                    if(currentbossid == 52300196 || currentbossid == 52300496)//sunflower
+                    {
+                        currentbossaddr = constants.FindAddrById(currentbossid);
+                        continue;
+                    }
+                    Console.WriteLine("Killed hitless");
+                    return true;
+                }
+                Console.WriteLine(isdead);
+                Thread.Sleep(500);
+
+            }
+            return false;
+        }
     }
-}
+}//2F00A 3 npc in metyr location
+ //524200190
+ //2F058 7 count ymir
