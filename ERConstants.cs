ee1e17bfffa3a48a324d0b648d9436ee0e7eeafe
Роман Эйрих <r.eirih@ya.ru>
2024-11-01 08:25:08 +0700

    Add new constant, update boss id's

--- ERConstants.cs
+++ ERConstants.cs
@@ -3,14 +3,16 @@
     public class ERConstants
     {
         private readonly ERMemoryReader er;
-        private long? cachedWorldchrman;
-        private long? cachedEventflagman;
+        private long? cachedWorldChrMan;
+        private long? cachedEventFlagMan;
+        private long? cachedGameDataMan;
+
 
         public ERConstants(ERMemoryReader er)
         {
             this.er = er;
             er.ProcessChanged += OnProcessChanged;
-            
+
         }
 
         private void OnProcessChanged(object? sender, EventArgs e)
@@ -20,13 +22,15 @@
 
         public void ResetCache()
         {
-            cachedWorldchrman = null;
-            cachedEventflagman = null;
+            cachedWorldChrMan = null;
+            cachedEventFlagMan = null;
+            cachedGameDataMan = null;
+
         }
 
         public long GetWorldchrman()
         {
-            if (!er.IsValid || !cachedWorldchrman.HasValue)
+            if (!er.IsValid || !cachedWorldChrMan.HasValue)
             {
                 byte[] pattern = new byte[]
                 {
@@ -37,14 +41,14 @@
                 if (er.ERProcess?.MainModule == null)
                     return 0;
                 long addr = er.FindPatternInModule(er.ERProcess.MainModule, pattern);
-                cachedWorldchrman = addr + er.ReadInt(addr + 3) + 7;
+                cachedWorldChrMan = addr + er.ReadInt(addr + 3) + 7;
             }
-            return cachedWorldchrman ?? 0;
+            return cachedWorldChrMan ?? 0;
         }
 
-        public long GetEventflagman()
+        public long GetEventFlagMan()
         {
-            if (!er.IsValid || !cachedEventflagman.HasValue)
+            if (!er.IsValid || !cachedEventFlagMan.HasValue)
             {
                 byte[] pattern = new byte[]
                 {
@@ -55,11 +59,27 @@
                 if (er.ERProcess?.MainModule == null)
                     return 0;
                 long addr = er.FindPatternInModule(er.ERProcess.MainModule, pattern);
-                cachedEventflagman = addr + er.ReadInt(addr + 3) + 7;
+                cachedEventFlagMan = addr + er.ReadInt(addr + 3) + 7;
             }
-            return cachedEventflagman ?? 0;
+            return cachedEventFlagMan ?? 0;
         }
-
+        public long GetGameDataMan()
+        {
+            if (!er.IsValid || !cachedGameDataMan.HasValue)
+            {
+                byte[] pattern = new byte[]
+                {
+                    0x48, 0x8B, 0x05,
+                    0x00, 0x00, 0x00, 0x00,
+                    0x48, 0x85, 0xC0, 0x74, 0x05, 0x48, 0x8B, 0x40, 0x58, 0xC3, 0xC3
+                };
+                if (er.ERProcess?.MainModule == null)
+                    return 0;
+                long addr = er.FindPatternInModule(er.ERProcess.MainModule, pattern);
+                cachedGameDataMan = addr + er.ReadInt(addr + 3) + 7;
+            }
+            return cachedGameDataMan ?? 0;
+        }
         public int GetChrCount()
         {
             if (!er.IsValid) return 0;
@@ -175,42 +195,21 @@
 
         public enum BossId
         {
-            Margit = 21300014,
-            Morgott = 21300534,
-            Rykard1 = 47100038,
-            Rykard2 = 47101038,
-            Godrick = 47500014,
-            Radahn = 47300040,
-            Renalla = 20310024,
-            RealMohg = 48000068,
-            Melania = 21200056,
-            RegalSpirit = 46700065,
-            Astel = 46200062,
-            Fortissax = 45110066,
-            Placidusax = 45200072,
-            FireGiant1 = 47600050,
-            FireGiant2 = 47601050,
-            Maliketh1 = 21100072,
-            Maliketh2 = 21101072,
-            Godfrey = 47200134,
-            EldenBeast = 22000078,
-            GodskinNobleVolcano = 35700038,
-            Mimic = 526100965,
-            SoldierOfGod = 43113906,
-            LimgraveTreeSentinel = 32510010,
-            Agheel = 45000010,
-            Nepheli = 533340014
-        }
+            Bayle = 51200085,
+            Lion = 52100088,
+            Rellana = 53000082,
+            Messmer1 = 51300099,
+            Messmer2 = 51301099,
+            Gay = 50000092,
+            Sunflower1 = 52300196,
+            Sunflower2 = 52300496,
+            Sunflower3 = 52300596,
 
+        }
+        //52300196 52300496 52300596
         public static readonly int[] REMEMBRANCE_BOSSES_PARAM_ID = {
-            (int)BossId.Godrick, (int)BossId.Renalla, (int)BossId.Radahn, (int)BossId.Morgott,
-            (int)BossId.Rykard1, (int)BossId.Rykard2, (int)BossId.RealMohg, (int)BossId.Melania,
-            (int)BossId.RegalSpirit, (int)BossId.Astel, (int)BossId.Fortissax, (int)BossId.Placidusax,
-            (int)BossId.FireGiant1, (int)BossId.FireGiant2, (int)BossId.Maliketh1, (int)BossId.Maliketh2,
-            (int)BossId.Godfrey, (int)BossId.EldenBeast
+            (int)BossId.Bayle, (int)BossId.Lion, (int)BossId.Rellana, (int)BossId.Messmer1, (int)BossId.Messmer2, (int)BossId.Gay, (int)BossId.Sunflower1, (int)BossId.Sunflower2, (int)BossId.Sunflower3,
         };
 
-        public const int MARGIT_PARRY_ANIM1 = 2008500;
-        public const int MARGIT_PARRY_ANIM2 = 2020015;
     }
 }
